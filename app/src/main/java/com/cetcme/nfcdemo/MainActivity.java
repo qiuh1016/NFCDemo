4f4888b7dec590b1ec18e84556caa14def520df9
qiuh1016 <qiuhong@qiuhs-iMac.local>
2016-07-28 13:05:16 +0800

    07281305

--- app/src/main/java/com/cetcme/nfcdemo/MainActivity.java
+++ app/src/main/java/com/cetcme/nfcdemo/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
 import android.os.Parcelable;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -24,25 +27,29 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView) findViewById(R.id.textView);
 
+        nfcCheck();
+
+    }
+
+    private void nfcCheck() {
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (nfcAdapter == null) {
             textView.setText("设备不支持NFC！");
-            finish();
             return;
         }
-        if (nfcAdapter != null && nfcAdapter.isEnabled()) {
+        if (!nfcAdapter.isEnabled()) {
             textView.setText("请在系统设置中先启功NFC功能！");
-            finish();
+            startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
             return;
         }
-
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-            readFromTag(getIntent());
+//            readFromTag(getIntent());
+            processIntent(getIntent());
         }
     }
 
@@ -62,4 +69,82 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
+
+    private String bytesToHexString(byte[] src) {
+        StringBuilder stringBuilder = new StringBuilder("0x");
+        if (src == null || src.length <= 0) {
+            return null;
+        }
+        char[] buffer = new char[2];
+        for (int i = 0; i < src.length; i++) {
+            buffer[0] = Character.forDigit((src[i] >>> 4) & 0x0F, 16);
+            buffer[1] = Character.forDigit(src[i] & 0x0F, 16);
+            System.out.println(buffer);
+            stringBuilder.append(buffer);
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Parses the NDEF Message from the intent and prints to the TextView
+     */
+    private void processIntent(Intent intent) {
+        //取出封装在intent中的TAG
+        Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        for (String tech : tagFromIntent.getTechList()) {
+            System.out.println(tech);
+        }
+        boolean auth = false;
+        //读取TAG
+        MifareClassic mfc = MifareClassic.get(tagFromIntent);
+        try {
+            String metaInfo = "";
+            //Enable I/O operations to the tag from this TagTechnology object.
+            mfc.connect();
+            int type = mfc.getType();//获取TAG的类型
+            int sectorCount = mfc.getSectorCount();//获取TAG中包含的扇区数
+            String typeS = "";
+            switch (type) {
+                case MifareClassic.TYPE_CLASSIC:
+                    typeS = "TYPE_CLASSIC";
+                    break;
+                case MifareClassic.TYPE_PLUS:
+                    typeS = "TYPE_PLUS";
+                    break;
+                case MifareClassic.TYPE_PRO:
+                    typeS = "TYPE_PRO";
+                    break;
+                case MifareClassic.TYPE_UNKNOWN:
+                    typeS = "TYPE_UNKNOWN";
+                    break;
+            }
+            metaInfo += "卡片类型：" + typeS + "\n共" + sectorCount + "个扇区\n共"
+                    + mfc.getBlockCount() + "个块\n存储空间: " + mfc.getSize() + "B\n";
+            for (int j = 0; j < sectorCount; j++) {
+                //Authenticate a sector with key A.
+                auth = mfc.authenticateSectorWithKeyA(j,
+                        MifareClassic.KEY_DEFAULT);
+                int bCount;
+                int bIndex;
+                if (auth) {
+                    metaInfo += "Sector " + j + ":验证成功\n";
+                    // 读取扇区中的块
+                    bCount = mfc.getBlockCountInSector(j);
+                    bIndex = mfc.sectorToBlock(j);
+                    for (int i = 0; i < bCount; i++) {
+                        byte[] data = mfc.readBlock(bIndex);
+                        metaInfo += "Block " + bIndex + " : "
+                                + bytesToHexString(data) + "\n";
+                        bIndex++;
+                    }
+                } else {
+                    metaInfo += "Sector " + j + ":验证失败\n";
+                }
+            }
+            textView.setText(metaInfo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
